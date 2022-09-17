f5ee5bae538160a01398952729ec83f4e50fe94f
Kalaganov5 <kalaganov5@gmail.com>
2022-09-17 17:37:52 +0400

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,186 +1,135 @@
-/* eslint-disable no-undef */
+/**
+ * Webpack main configuration file
+ */
+
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-const isProduction = !isDevelopment;
+const environment = require('./configuration/environment');
 
-const optimization = () => {
-    const config = {
-        splitChunks: {
-            chunks: 'all'
-        }
-    };
+const templateFiles = fs.readdirSync(environment.paths.source)
+  .filter((file) => ['.html', '.ejs'].includes(path.extname(file).toLowerCase())).map((filename) => ({
+    input: filename,
+    output: filename.replace(/\.ejs$/, '.html'),
+  }));
 
-    if (isProduction) {
-        config.minimizer = [
-            new OptimizeCssAssetsWebpackPlugin(),
-            new TerserWebpackPlugin()
-        ]
-    }
-
-    return config;
-};
-
-const filename = ext => isDevelopment ? `[name].${ext}` : `[name].[hash].${ext}`;
-
-const cssLoaders = (extra) => {
-    const loaders = [
-        {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-                hmr: isDevelopment,
-                reloadAll: true
-            }
-        }, 'css-loader'
-    ];
-
-    if (extra) {
-        loaders.push(extra);
-    }
-
-    return loaders;
-};
-
-const babelOptions = preset => {
-
-    const options = {
-        presets: [
-            '@babel/preset-env'
-        ],
-        plugins: [
-            '@babel/plugin-proposal-class-properties'
-        ]
-    };
-
-    if (preset) {
-        options.presets.push(preset);
-    }
-
-    return options;
-};
-
-const jsLoaders = () => {
-  const loaders = [{
-      loader: 'babel-loader',
-      options: babelOptions()
-  }];
-
-  if (isDevelopment) {
-      loaders.push('eslint-loader');
-  }
-
-  return loaders;
-};
-
-const plugins = () => {
-    const base = [
-        new HTMLWebpackPlugin({
-            template: './index.html',
-            minify: {
-                collapseWhitespace: isProduction
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, 'src/favicon.ico'),
-                to: path.resolve(__dirname, 'dist')
-            }
-        ]),
-        new MiniCssExtractPlugin({
-            filename: filename('css')
-        })
-    ];
-
-    if (isProduction) {
-        base.push(new BundleAnalyzerPlugin());
-    }
-
-    return base;
-};
+const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
+  inject: true,
+  hash: false,
+  filename: template.output,
+  template: path.resolve(environment.paths.source, template.input),
+  favicon: path.resolve(environment.paths.source, 'images', 'favicon.ico'),
+}));
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: {
-        main: ['@babel/polyfill', './index.js'],
-    },
-    output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js', '.json', '.png'],
-        alias: {
-            '@models': path.resolve(__dirname, 'src/models'),
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    optimization: optimization(),
-    devServer: {
-        port: 8888,
-        hot: isDevelopment
-    },
-    devtool: isDevelopment ? 'source-map' : '',
-    plugins: plugins(),
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: cssLoaders()
-            },
-            {
-                test: /\.less$/,
-                use: cssLoaders('less-loader')
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: cssLoaders('sass-loader')
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.xml$/,
-                use: ['xml-loader']
-            },
-            {
-                test: /\.csv$/,
-                use: ['csv-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: jsLoaders()
-            },
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                loader: {
-                    loader: 'babel-loader',
-                    options: babelOptions('@babel/preset-typescript')
-                }
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                loader: {
-                    loader: 'babel-loader',
-                    options: babelOptions('@babel/preset-react')
-                }
-            }
-        ]
-    }
+  entry: {
+    app: path.resolve(environment.paths.source, 'js', 'app.js'),
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: environment.paths.output,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|gif|jpe?g|svg)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: environment.limits.images,
+          },
+        },
+        generator: {
+          filename: 'images/design/[name].[hash:6][ext]',
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: environment.limits.images,
+          },
+        },
+        generator: {
+          filename: 'images/design/[name].[hash:6][ext]',
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      '...',
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              // Svgo configuration here https://github.com/svg/svgo#configuration
+              [
+                'svgo',
+                {
+                  plugins: [
+                    {
+                      name: 'removeViewBox',
+                      active: false,
+                    },
+                  ],
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new CleanWebpackPlugin({
+      verbose: true,
+      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(environment.paths.source, 'images', 'content'),
+          to: path.resolve(environment.paths.output, 'images', 'content'),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+        {
+          from: path.resolve(environment.paths.source, 'videos'),
+          to: path.resolve(environment.paths.output, 'videos'),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+      ],
+    }),
+  ].concat(htmlPluginEntries),
+  target: 'web',
 };
