dd362af6c81a6a9f18c5a03423a40dbfd29f5e03
kalaganov5 <kalaganov5@gmail.com>
2022-10-14 11:02:17 +0300

    form style

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import '../scss/app.scss';
 
 /* Your JS Code goes here */
 import { Navigation } from './navigation/navigation.js';
+import { ContactForm7Handler } from './contact-form7.js';
 
 const navigation = new Navigation();
 navigation.init();
+
+const form = new ContactForm7Handler('.wpcf7');
+form.init();
